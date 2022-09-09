7114ef9db9554f332dae3048cb5cce2af19cf1db
Alexey-Kushnir <al7787@gmail.com>
2022-09-09 14:24:44 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imgMarkup = createGallaryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createGallaryItem(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+  <div class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+  </div>
+  `;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+// event.preventDefault();
+
+function onGalleryContainerClick(e) {
+  console.log(e.target);
+  event.preventDefault();
+}
